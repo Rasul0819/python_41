a6ebde15d8f8ce956b6ba1e42c9dd764373b0e74
Rasul0819 <rasulabdikerimov87@gmail.com>
2025-01-14 10:33:29 +0500

    birinshi commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# from aiogram import tipes,bot,Dispetcher
+
+# api = ''
+# bot = bot(api)
+# dp = Dispetcher(bot)
+
+# @db.message_xandler(commands=['start'])
+# async def send_hi(sms:tipes.Message):
+#     await sms.answer(text='Salem',
+#                      reply_markup='main_menu')
+# @dp.message_handler(kommands=['photo'])
+# async def send_ph(sms:tipes.Message):
+#     file = close(file='lion.jpg',mode='rb')
+#     await sms.answer_photo(
+#         photo='Bul lion',
+#         caption=file
+#     )
+
+
+# if __name__='__main__':
+#     executor.start_polling(dp,skip_updates=True)
+
+
+# import customtkinter
+# window = customtkinter.CTk()
+# window.title('Game')
+# window.geometry('500x500')
+# salem = customtkinter.CTkLabel(
+#     master=window,
+#     text='Salem',
+#     font=('Arial',20)
+# )
+# salem.pack()
+# knopka = customtkinter.CTkButton(
+#     master=window,
+#     text='Hello',
+#     font=('Arial',20)
+# )
+# knopka.pack()
+# window.mainloop()
